418222cd51b7d5daa803b2e39f871402632b672e
AYOUB <a.idihia@gmail.com>
2023-05-13 13:26:12 +0200

    Changing name

--- src/main/java/io/awiya/Main.java
+++ src/main/java/io/awiya/Main.java
@@ -1,5 +1,7 @@
-package io.awiya;public class Main {
+package io.awiya;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Ayoub!");
     }
 }
